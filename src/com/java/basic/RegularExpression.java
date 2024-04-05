394bc631a0423792978249414d90fc55c1eb32ce
池原 実夢 <m_ikehara@sharecrest.com>
2024-04-05 18:29:30 +0900

    Java基礎 - Lesson1 - 文字列の操作 追加 :gitにcommitしました

---
+++ src/com/java/basic/RegularExpression.java
@@ -0,0 +1,10 @@
+package com.java.basic;
+
+public class RegularExpression {
+	public static void main(String[] args) {
+		
+		String myNumber = "My cellphone number is 010-1234-5678.";
+		System.out.println(myNumber.replaceAll("[1-9]", "X"));
+	}
+
+}
