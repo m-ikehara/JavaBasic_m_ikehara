85ee29e313bd1d6fe223c10cf94d4610209e582c
池原 実夢 <m_ikehara@sharecrest.com>
2024-04-05 17:59:53 +0900

    Java基礎 - Lesson1 - 文字列の操作 追加 :gitにcommitしました

--- src/com/java/basic/StringFunction.java
+++ src/com/java/basic/StringFunction.java
@@ -35,6 +35,8 @@ public class StringFunction {
 		}
 		
 		System.out.println("(5)");
+		System.out.println(String.join(",", letter6));
+		System.out.println(String.join(" ", letter6));
 		
 		System.out.println("(6)");
 		
