7a38436524bf544e9460dcfcbe925de091ea36e5
池原 実夢 <m_ikehara@sharecrest.com>
2024-04-08 16:51:08 +0900

    Java基礎 -Lesson3 - オブジェクト指向 追加 : gitにcommitしました

---
+++ src/com/java/basic/ObjectMain1.java
@@ -0,0 +1,27 @@
+package com.java.basic;
+
+public class ObjectMain1 {
+
+	public static void main(String[] args) {
+		
+		int x = 10;
+		int y = 7;
+		
+		
+		//インスタンス化しているよ！
+		Calculator calcuA = new Calculator();
+		Calculator calcuB = new Calculator();
+		
+		calcuA.init(x);
+		calcuA.add(y);
+		calcuA.div(2);
+		
+		calcuB.init(x);
+		calcuB.add(y);
+		calcuB.sqrt();
+		
+		System.out.println("計算機Aの結果 :" + calcuA.getResult());
+		System.out.println("計算機Bの結果 :" + calcuB.getResult());
+	}
+
+}
