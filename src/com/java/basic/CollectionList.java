27f7a0a4627417fc7d30aaf58fa5bdc1a589eb87
池原 実夢 <m_ikehara@sharecrest.com>
2024-04-08 13:10:02 +0900

    Java基礎 -Lesson2 - コレクション 追加 : gitにcommitしました

--- src/com/java/basic/CollectionList.java
+++ src/com/java/basic/CollectionList.java
@@ -1,9 +1,31 @@
 package com.java.basic;
 
+import java.util.ArrayList;
+
 public class CollectionList {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		
+		
+		ArrayList<String> fruits = new ArrayList<String>();
+		
+		fruits.add("リンゴ");
+		fruits.add("バナナ");
+		fruits.add("キウイ");
+		fruits.add("ブドウ");
+		fruits.add("リンゴ");
+		fruits.add("カボチャ");
+		fruits.add("トマト");
+		fruits.add("ニンジン");
+		
+		fruits.set(5,"ナシ");
+		
+		fruits.remove(7);
+		fruits.remove("トマト");
+		
+		for(int i = 0; i < fruits.size(); i++) {
+			System.out.println("fruitsリスト :" + fruits.get(i));
+		}
 
 	}
 
