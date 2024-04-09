8a89b458efca339f9bac1eca9aa440fe351ce25c
池原 実夢 <m_ikehara@sharecrest.com>
2024-04-09 16:25:10 +0900

    Java基礎 - Lesson４ - 継承　抽象クラス追加:gitにcommitしました

---
+++ src/com/java/basic/Mammalian.java
@@ -0,0 +1,11 @@
+package com.java.basic;
+
+public abstract class Mammalian {
+	
+	abstract public String move();
+	
+	abstract public String eat();
+	
+	abstract public String respiration();
+
+}
---
+++ src/com/java/basic/ObjectMain3.java
@@ -0,0 +1,10 @@
+package com.java.basic;
+
+public class ObjectMain3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
