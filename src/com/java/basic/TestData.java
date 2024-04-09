8428193942483209911ea5f9ca80968e9cd8b1ec
池原 実夢 <m_ikehara@sharecrest.com>
2024-04-09 12:26:22 +0900

    Java基礎 - Lesson3 - オブジェクト指向　まとめと練習問題　追加:gitにcommitしました

---
+++ src/com/java/basic/TestData.java
@@ -0,0 +1,46 @@
+package com.java.basic;
+
+public class TestData {
+
+	static int DEAD_LINE = 35;
+
+	// メンバ変数の定義
+	private int score;
+	private String name;
+
+	// コンストラクタ
+	public TestData(String name) {
+		this.name = name;
+	}
+
+	public TestData(int score) {
+		this.score = score;
+	}
+
+	// getterとsetterを定義
+	public int getScore() {
+		return this.score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// 合否の取得メソッド
+	public String getResult() {
+		if (this.score >= DEAD_LINE) {
+			return "合格";
+		} else {
+			return "不合格";
+		}
+	}
+
+}
