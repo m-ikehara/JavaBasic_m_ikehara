25849b9d5a60cdec3728cfb2b4592773f70db693
池原 実夢 <m_ikehara@sharecrest.com>
2024-04-10 13:09:15 +0900

    Java基礎 - Lesson5 - try-catch文 追加:gitにcommitしました

--- src/com/java/basic/JavaException.java
+++ src/com/java/basic/JavaException.java
@@ -5,11 +5,21 @@ public class JavaException {
 	public static void main(String[] args) {
 		
 		String str = null;
+		int a = 10;
+		int b = 2;
+		int result = 0;
 		
 		try {
+			result = a/b;
+			System.out.println(result);
 			System.out.println(str.length());
+			
 		}catch(NullPointerException e) {
-			System.out.println("エラーが発生しました。");
+			System.out.println("NullPointExceptionエラーが発生しました。");
+			e.printStackTrace();
+		}catch(ArithmeticException e) {
+			System.out.println("AeithmeticExceptionエラーが発生しました。");
+			e.printStackTrace();
 		}
 	}
 
