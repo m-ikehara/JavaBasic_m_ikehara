e094254c110053b6d3744f3b7337d51ee9fa2e5b
池原 実夢 <m_ikehara@sharecrest.com>
2024-04-11 17:19:24 +0900

    Java基礎 - Lesson7 -　ファイル　ストリームとフィルタ　追加:gitにcommitしました

---
+++ src/com/java/basic/BinaryFileWriting.java
@@ -0,0 +1,27 @@
+package com.java.basic;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class BinaryFileWriting {
+
+	public static void main(String[] args) {
+		
+		try {
+			File binaryFile = new File("C:￥￥java-basic\\test\\abc.dat");
+			binaryFile.createNewFile();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		try(FileOutputStream fos = new FileOutputStream("C:￥￥java-basic\\test\\abc.dat");){
+			
+			byte[] data = {0b1111111,0b1010101,0b0000111};
+			fos.write(data);
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
