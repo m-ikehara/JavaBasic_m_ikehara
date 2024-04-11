27d18a2cc1d56e3cea791cf901b372aedffabca8
池原 実夢 <m_ikehara@sharecrest.com>
2024-04-11 09:52:11 +0900

    Java基礎 - Lesson6 - インナークラスとｅｎｍ型 追加:gitにcommitしました

---
+++ src/com/java/basic/InnerClass.java
@@ -0,0 +1,11 @@
+package com.java.basic;
+
+public class InnerClass {
+
+	public static void main(String[] args) {
+		MobilePhone yamadaphone = new MobilePhone("山田","080-1111-2222");
+		yamadaphone.sendMail("高橋","お久しぶりです。\n お元気ですか？");
+
+	}
+
+}
