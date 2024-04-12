f93a9b044fcfd4163910e657f1061fb5b73d3c95
池原 実夢 <m_ikehara@sharecrest.com>
2024-04-12 10:09:57 +0900

    Java基礎 - Lesson7 -　ファイル　まとめと練習問題　追加:gitにcommitしました

--- src/com/java/basic/Lesson7.java
+++ src/com/java/basic/Lesson7.java
@@ -3,6 +3,7 @@ package com.java.basic;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Lesson7 {
@@ -10,18 +11,20 @@ public class Lesson7 {
 	public static void main(String[] args) {
 		
 		// ディレクトリのパス
+		File dir = new File("C:\\java-basic\\lesson7");
 		//　ディレクトリのパス
+		File file = new File("C:\\java-basic\\lesson7\\months.txt");
 		//　dirのファイルパスのディレクトリを作成するコードを記載
-		
+		dir.mkdirs();
 		try {
 			//fileのファイルパスのファイルを作成するコードを記載
-		}catch() {
-			
-			
+			file.createNewFile();
+		}catch(IOException e) {
+			System.out.println("テキストファイルの作成に失敗しました。");
 		}
 		
-		//ファイルの書き込み
-		try ()){
+		//ファイルの書き込み:ストリームfwのインスタンスを生成
+		try (FileWriter fw  = new FileWriter("C:\\java-basic\\lesson7\\months.txt",true);){
 			fw.write("January \n");
 			fw.write("Febuary \n");
 			fw.write("March \n");
@@ -38,14 +41,14 @@ public class Lesson7 {
 			System.out.println("テキストファイルの書き込みに失敗しました。");
 		}
 		
-		//ファイルの読み込み
-		try()){
+		//ファイルの読み込み:バッファリングフィルタで読み込む、いったんふつうのやついこか
+		try(FileReader fr = new FileReader("C:\\java-basic\\lesson7\\months.txt")){
 			BufferedReader br = new BufferedReader(fr);
-			String line = ; //1行読み込むコードを記述
+			String line = br.readLine(); //1行読み込むコードを記述
 			int month = 1;
 			while(line != null) {
 				System.out.println(month + "月:" + line);
-				line = ; //1行読み込むコードを記述
+				line = br.readLine(); //1行読み込むコードを記述
 				month++;
 			}
 		}catch(IOException e) {
